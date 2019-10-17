bdb3f75a109962361333a79fb375ef9c2da0ca48
LEMON-ell0214 <2693666456@qq.com>
2019-10-17 11:47:23 +0800

    jswork

---
+++ work5/work5.js
@@ -0,0 +1,32 @@
+let str = '<table border="1">'
+for(let i=9;i<18;i++){
+    str += '<tr>'
+    for(var j=1;j<=9;++j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML =str
+
+str = '<table border="2">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table2').innerHTML =str
+
+str = '<table border="3">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>'+ j + '*' +i +'=' +(j*i)+'</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML =str
