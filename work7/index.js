1443438b59324d78c39732b591ff8241e97d9eef
LEMON-ell0214 <2693666456@qq.com>
2019-10-26 01:45:33 +0800

    work7

--- work7/index.js
+++ work7/index.js
@@ -2,7 +2,7 @@ function sort(method){
     let origin = document.getElementById('origin').value
     let logs
     console.log(method)
-    if(method=='dubble'){
+    if(method=='bubble'){
         logs = bubbleSort(origin)
     }else if(method=='insert'){
         logs = insertSort(origin)
--- work7slide/index.js
+++ work7slide/index.js
@@ -2,14 +2,14 @@ function sort(method){
     let origin = document.getElementById('origin').value
     let logs
     console.log(method)
-    if(method=='dubble'){
+    if(method=='bubble'){
         logs = bubbleSort(origin)
     }else if(method=='insert'){
         logs = insertSort(origin)
     }
     showDiv = document.getElementById('Anim')
-    logValues = logs.values()
+     //生成排序日志数组的遍历器对象
+     logValues = log.values()
     console.log(logValues.next())
-    setTimeout("showLog(logVaule,showDiv)",50)
-    showDiv.innerHTML = logs
+    setTimeout("showLog(logValues,showDiv)", 50)
 }
