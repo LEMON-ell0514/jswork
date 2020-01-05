f868da88594a15ae552be3d648482d1541271905
LEMON-ell0214 <2693666456@qq.com>
2020-01-05 14:25:37 +0800

    work22

--- work18/index.js
+++ work18/index.js
@@ -11,8 +11,9 @@ function send() {
     xmlhttp.open('post', 'http://localhost:8080/ajax', true)
     //xmlhttp.open('post', 'http://139.9.81.203:8090/ajax', true)
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send('sno=' + sno + '&name=' + name + "&content=" + content)
 
+    
+    
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             let ajaxData = JSON.parse(xmlhttp.responseText)
